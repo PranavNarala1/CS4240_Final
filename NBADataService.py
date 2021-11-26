8336a65ddb8d2bdbb2176e1e33c6a2e78ef7e6bd
PranavNarala1 <56051957+PranavNarala1@users.noreply.github.com>
2021-11-25 19:54:18 -0500

    Debugging

--- NBADataService.py
+++ NBADataService.py
@@ -3,7 +3,7 @@ import numpy as np
 
 #Data Year Range: (2005-06) to (2017-18)
 
-#Account for team name changes (ex. Bobcats to Hornets)
+#Account for team name changes (ex. Bobcats to Hornets)------------------------------
 abbreviation_to_name = {
 'ATL'	: 'Atlanta Hawks',
 'BOS'	: 'Boston Celtics',
@@ -14,15 +14,18 @@ abbreviation_to_name = {
 'DEN'	: 'Denver Nuggets',
 'DET'	: 'Detroit Pistons',
 'GSW'	: 'Golden State Warriors',
-'HOU' : 'Houston Rockets',
+'HOU'   : 'Houston Rockets',
 'IND'	: 'Indiana Pacers',
-'LAC' : 'Los Angeles Clippers',
+'LAC'   : 'Los Angeles Clippers',
 'LAL'	: 'Los Angeles Lakers',
 'MEM'	: 'Memphis Grizzlies',
 'MIA'	: 'Miami Heat',
 'MIL'	: 'Milwaukee Bucks',
 'MIN'	: 'Minnesota Timberwolves',
-'NOH'	: 'New Orleans Pelicans',
+'NJN'   : 'New Jersey Nets',
+'NOH'   : 'New Orleans Hornets',
+'NOK'   : 'New Orleans/Oklahoma City Hornets',
+'NOP'	: 'New Orleans Pelicans',
 'NYK'	: 'New York Knicks',
 'BKN'	: 'Brooklyn Nets',
 'OKC'	: 'Oklahoma City Thunder',
@@ -31,6 +34,7 @@ abbreviation_to_name = {
 'PHO'	: 'Phoenix Suns',
 'POR'	: 'Portland Trail Blazers',
 'SAC'	: 'Sacramento Kings',
+'SEA'   : 'Seattle Supersonics',
 'TOR'	: 'Toronto Raptors',
 'UTH'	: 'Utah Jazz',
 'WAS'	: 'Washington Wizards'
@@ -54,8 +58,11 @@ def get_defensive_rating(team_name, year):
         if row[1][1].split()[-1].lower() == team_name.lower() and row[1][0] == year:
             return row[1][3]
 
-def to_team_name(abbreviation):
-    return abbreviation_to_name[abbreviation]
+def to_team_name(abbreviation, year):
+    if (year == '2013-14' or int(year[5:]) <= 13) and abbreviation == 'CHA':
+        return "Bobcats"
+    else:
+        return abbreviation_to_name[abbreviation].split()[-1]
 
 def get_x_train():
     x_train_list = []
@@ -65,8 +72,8 @@ def get_x_train():
         for row in year_data.iterrows():
             #Prevents double addition of games into train_list
             if row[1][1].split()[1] == 'vs.':
-                home_team = to_team_name(row[1][1].split()[0])
-                away_team = to_team_name(row[1][1].split()[2])
+                home_team = to_team_name(row[1][1].split()[0], year)
+                away_team = to_team_name(row[1][1].split()[2], year)
                 x_train_list.append([
                         get_win_percentage(home_team, year),
                         get_win_percentage(away_team, year),
@@ -76,7 +83,6 @@ def get_x_train():
                         get_defensive_rating(away_team, year)
                         ])
 
-    print(x_train_list)
     return np.array(x_train_list)
 
 
@@ -90,15 +96,10 @@ def get_y_train():
             #Prevents double addition of games into train_list
             if row[1][1].split()[1] == 'vs.':
                 if row[1][3] == 'W':
-                    y_train_list.append([
-                        1
-                    ])
+                    y_train_list.append(1)
                 else:
-                    y_train_list.append([
-                        0
-                    ])
+                    y_train_list.append(0)
 
-    print(y_train_list)
     return np.array(y_train_list)
 
 #Standardize data later so that offensive and defensive ratings are values from 0-1
@@ -108,5 +109,4 @@ def get_x_test(away_team, home_team, year):
             get_defensive_rating(home_team, year), get_defensive_rating(away_team, year)]
 
 print(get_x_train())
-print(get_y_train())
-print(get_x_test('Spurs', 'Mavericks', '2009-10'))
+#print(get_y_train())
--- Testing.py
+++ Testing.py
@@ -1,11 +1,11 @@
 #This class will be used for testing purposes.
 
-#import Model
+import Model
 import NBADataService
 
-print(NBADataService.get_offensive_rating("Mavericks", "2011-12"))
-print(NBADataService.get_defensive_rating("Nuggets", "2009-10"))
+#print(NBADataService.get_offensive_rating("Mavericks", "2011-12"))
+#print(NBADataService.get_defensive_rating("Nuggets", "2009-10"))
 
-#test_model = Model()
-#test_model.train()
-#test_model.analyze_training()
+test_model = Model()
+test_model.train()
+test_model.analyze_training()
