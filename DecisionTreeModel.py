c9372b5c575c439ed0a696e0b9d4e556a6006710
PranavNarala1 <56051957+PranavNarala1@users.noreply.github.com>
2021-11-27 17:52:29 -0500

    Adding a DecisionTreeModel

---
+++ DecisionTreeModel.py
@@ -0,0 +1,24 @@
+import NBADataService
+from sklearn import tree
+import joblib
+
+class DecisionTreeModel(object):
+
+    def __init__(self):
+        self.tree_model = tree.DecisionTreeClassifier()
+
+    def train(self):
+        x_train = NBADataService.get_x_train()
+        y_train = NBADataService.get_y_train()
+        self.tree_model = self.tree_model.fit(x_train, y_train)
+
+    def make_prediction(self, team_1, team_2, year):
+        x_test = NBADataService.get_x_test(team_1, team_2, year)
+        tree_prediction = self.tree_model.predict(x_test)
+        return tree_prediction
+
+    def save_model(self, file_path):
+        joblib.dump(self.model, file_path)
+
+    def load_model(self, file_path):
+        self.model = joblib.load(file_path)
--- Testing.py
+++ Testing.py
@@ -1,14 +1,14 @@
 #This file will be used for testing purposes.
 
-from Model import Model
+#from Model import Model
+from DecisionTreeModel import DecisionTreeModel
 
-
-#test_model = Model()
-#test_model.train()
-#test_model.save_model('test_model_trained_with_all_13_seasons')
+test_tree_model = DecisionTreeModel()
+test_tree_model.train()
+test_tree_model.save_model('decision_tree_model')
 #test_model.analyze_training()
 #print(test_model.analyze_training())
 
-test_model_trained_with_all_13_seasons = Model()
-test_model_trained_with_all_13_seasons.load_model('test_model_trained_with_all_13_seasons')
-print(test_model_trained_with_all_13_seasons.make_prediction('Warriors', 'Cavaliers', '2015-16'))
+#test_model_trained_with_all_13_seasons = Model()
+#test_model_trained_with_all_13_seasons.load_model('test_model_trained_with_all_13_seasons')
+#print(test_model_trained_with_all_13_seasons.make_prediction('Warriors', 'Cavaliers', '2015-16'))
