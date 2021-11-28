08d22dee7e9a0b1be8cc2f78f9f100c6030658ef
PranavNarala1 <56051957+PranavNarala1@users.noreply.github.com>
2021-11-28 18:58:28 -0500

    Creating RandomForestModel

--- Testing.py
+++ Testing.py
@@ -1,7 +1,9 @@
 #This file will be used for testing purposes.
 
-from Model import Model
-from DecisionTreeModel import DecisionTreeModel
+#from Model import Model
+#from DecisionTreeModel import DecisionTreeModel
+
+from RandomForestModel import RandomForestModel
 
 #test_tree_model = DecisionTreeModel()
 #test_tree_model.train()
@@ -13,10 +15,14 @@ from DecisionTreeModel import DecisionTreeModel
 #test_tree_model.load_model('decision_tree_model')
 #print(test_tree_model.make_prediction('Warriors', 'Cavaliers', '2015-16'))
 
-model = Model()
-model.load_model('test_model')
-print(model.test_model())
+#model = Model()
+#model.load_model('test_model')
+#print(model.test_model())
 
-test_tree_model = DecisionTreeModel()
-test_tree_model.load_model('decision_tree_model')
-print(test_tree_model.test_model())
+#test_tree_model = DecisionTreeModel()
+#test_tree_model.load_model('decision_tree_model')
+#print(test_tree_model.test_model())
+
+random_forest_model = RandomForestModel()
+random_forest_model.train()
+random_forest_model.save_model('random_forest_model')
