83b0bddfaa047476efef09c5393d1374a4ba3deb
PranavNarala1 <56051957+PranavNarala1@users.noreply.github.com>
2021-12-01 17:35:09 -0500

    Documentation

--- Testing.py
+++ Testing.py
@@ -1,9 +1,15 @@
 #This file will be used for testing purposes.
 
-#from Model import Model
+from Model import Model
+#from RandomForestModel import RandomForestModel
 #from DecisionTreeModel import DecisionTreeModel
+import NBADataService
+import numpy as np
 
-from RandomForestModel import RandomForestModel
+
+print(type(NBADataService.get_x_test('Heat', 'Lakers', '2008-09')))
+
+#print(type(NBADataService.to_team_name('CHA', '2013-14')))
 
 #test_tree_model = DecisionTreeModel()
 #test_tree_model.train()
@@ -16,13 +22,22 @@ from RandomForestModel import RandomForestModel
 #print(test_tree_model.make_prediction('Warriors', 'Cavaliers', '2015-16'))
 
 #model = Model()
-#model.load_model('test_model')
+#model.load_model('test_model_trained_with_all_13_seasons')
+
+#print(model.make_prediction('Pacers', 'Heat', '2012-13'))
+
+#model.load_model('test_model_trained_with_all_13_seasons')
+
 #print(model.test_model())
 
 #test_tree_model = DecisionTreeModel()
 #test_tree_model.load_model('decision_tree_model')
 #print(test_tree_model.test_model())
 
-random_forest_model = RandomForestModel()
-random_forest_model.train()
-random_forest_model.save_model('random_forest_model')
+#random_forest_model = RandomForestModel()
+#random_forest_model.train()
+#random_forest_model.save_model('random_forest_model')
+
+#random_forest_model = RandomForestModel()
+#random_forest_model.load_model('random_forest_model')
+#print(random_forest_model.test_model())
